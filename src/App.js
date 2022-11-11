7edec1b26d33be29ae278ff106a4841d39d1f631
sleepypenguin763 <kenya_kimata+1300@brown.edu>
2022-11-11 17:13:55 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,61 +1,87 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 import { useEffect, useState } from 'react';
+import jsonFlightData from "./assets/data.json";
 
-// Make a GET request to the fruityvice api to retrieve some fruit data
-const PROXY_URL = 'https://cs1300-cors-anywhere.herokuapp.com/'
-const buildProxyEndpoint = (endpoint) => `${PROXY_URL}${endpoint}`;
-const apiRequest = async () => {
-  const BASE_URL = 'https://www.fruityvice.com/api/'
-
-  // This endpoint (https://www.fruityvice.com/doc/index.html#api-GET-getAll) returns a list of all the fruits and their info, feel free to play around with different endpoints!
-  const resourcePath = 'fruit/all'
-
-  // Making a fetch request to an API endpoint
-  // Note: a fetch request is an asynchronous operation, and `await` tells the program to wait until the request has been completed before continuing
-  const endpoint = BASE_URL + resourcePath;
-  const response = await fetch(buildProxyEndpoint(endpoint), {
+const apiRequest = async(path) => {
+  const BASE_URL = "https://opensky-network.org/api/";
+  const currFlightPath = path == null ? "states/all" : path;
+  const endpoint = BASE_URL + currFlightPath;
+  const response = await fetch(endpoint, {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
     }
-  });
-
-  // console.log(response);
-
-  // Return the response in JSON format
+  })
   return response.json();
 }
 
+const saveDataAsJSON = async(flights) => {
+  const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+    JSON.stringify(flights)
+  )}`;
+  const link = document.createElement("a");
+  link.href = jsonString;
+  link.download = "data.json";
+
+  link.click();
+};
+
+
+//Call this inside useEffect
+const getFlightRouteData = async(flightsData) =>{
+  const url = 'https://sleepypenguin763.github.io/FlightData/routes/AAC.json';
+
+  const response = await fetch(url);
+  let queryResult = "11111"
+  await response.json().then(json => queryResult = json);
+  
+  //const cleanData = flightsData.states.map(async(flight) => {
+   // const callsign = flight[1];
+    //return null;
+  //});
+  return queryResult;
+}
+
 function App() {
-  const [fruits, setFruits] = useState([]);
-  const [allFruits, setAllFruits] = useState([]);
+  const [flights, setFlights] = useState([]);
+  const [routes, setRoutes] = useState([]);
   useEffect(() => {
-    const fruitsArray = async() => {
+    /*
+    const getFlightsFromAPI = async() => {
       const resp = await apiRequest();
-      setAllFruits(resp);
-      const sugaryFruits = await resp.filter((fruit) => {return fruit.nutritions.sugar > 5});
-      setFruits(sugaryFruits);
+      setFlights(resp);
     };
-    fruitsArray();
+    getFlightsFromAPI();*/
+    const getFlightsFromJSON = jsonFlightData;
+    setFlights(getFlightsFromJSON);
+    const getFlightRoute = async() => {
+      const resp = await getFlightRouteData(flights);
+      setRoutes(resp);
+    };
+    getFlightRoute();
+    console.log(routes["AAC710"]["AirportCodes"]);
   }, []);
 
-  const changeList = async() => {
-    const newFilter = await allFruits.filter((fruit) => {return fruit.nutritions.sugar < 5});
-    setFruits(newFilter);
-  };
+ const data = flights.states;
+ //https://opensky-network.org/api/tracks/all?icao24=n71lp&time=0
+ //https://opensky-network.org/api/routes?callsign=UAL5
+ //https://opensky-network.org/api/airports?icao=KIAH
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
-      {fruits.map((fruit) => ( // TODO: map bakeryData to BakeryItem components
-        // replace with BakeryItem component
+      <h1>Flight Description</h1>
+      {data == null ? <div>Please wait for the data to load</div> : data.map((flight) => (
         <div>
-          <h1>{fruit.name} ({fruit.nutritions.sugar})</h1>
+          <h1>CallSign: {flight[1]}</h1>
+          <p>Origin: {flight[2]}</p>
+          <p>Velocity: {(parseFloat(flight[9]) * 3.6).toFixed(3)} km/h</p>
+          <p>Geographic Altitude: {flight[13]}</p>
+          <br></br>
         </div>
       ))}
-      <button onClick={() => changeList()}> test </button>
+
+      <button onClick={() => saveDataAsJSON(flights)}> Save RealTime JSON Data </button>
     </div>
   );
 }
