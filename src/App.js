d3442a1f53fb833310ae4f83417d05cc489c4804
sleepypenguin763 <kenya_kimata+1300@brown.edu>
2022-11-16 14:37:34 -0500

    added route data

--- src/App.js
+++ src/App.js
@@ -52,11 +52,11 @@ const getAirlineData = async(callsign, resp) => {
     return null;
   }
   const firstThreeChar = callsign.slice(0, 3);
-  const url = 'https://sleepypenguin763.github.io/Aviation/airlines/' + firstThreeChar;
+  const url = 'https://sleepypenguin763.github.io/Airlines/airlines/' + firstThreeChar;
   const response = await fetch(url);
   if (response.status === 404){
     const firstTwoChar = callsign.slice(0, 2);
-    const url2 = 'https://sleepypenguin763.github.io/Aviation/airlines/' + firstTwoChar;
+    const url2 = 'https://sleepypenguin763.github.io/Airlines/airlines/' + firstTwoChar;
     const response2 = await fetch(url2);
     if (response2.status === 404){
       return null;
@@ -64,6 +64,27 @@ const getAirlineData = async(callsign, resp) => {
     return await response2.json();
   }
   return await response.json();
+}
+
+const getAirportData = async(resp) => {
+  if (resp === undefined || resp === null) {
+    return null;
+  }
+  const route = resp["AirportCodes"];
+  const split = route.split('-');
+  const airportData = split.map(async(airport) => {
+    const fullAirportCode = airport.slice(0, 4);
+    const firstTwoChar = airport.slice(0, 2);
+    const url = 'https://sleepypenguin763.github.io/Airlines/airports/' + firstTwoChar + '/' + fullAirportCode;
+    const response = await fetch(url);
+    if (response.status == 404) {
+      return null;
+    }
+    return await response.json();
+  });
+  return await Promise.all(airportData).then(out => {
+    return out;
+  });
 }
 
 function CurrentAirplenStatus({data}) {
@@ -89,12 +110,29 @@ function CurrentAirplenStatus({data}) {
           }
         }
       }
+
+      const airportData = (flight["airportData"] === null) ? null : flight["airportData"];
+      let airportNames = "";
+      if (airportData != null) {
+        for (var c in airportData){
+          if (airportData[c] === null || airportData[c] === undefined || airportData[c] === ""){
+            airportNames += "Airport not found";
+          }else{
+            airportNames += airportData[c]["Location"];
+          }
+          if (c < airportData.length - 1){
+            airportNames += " => "
+          }
+        }
+      }
+
       const invalidCallsign = callsign === null || callsign === undefined || callsign === "";
 
       return (<div key={index}>
         <h1>CallSign: {invalidCallsign ? "Undefined" : callsign}</h1>
         <p>Airline: {airline == null ? "Airline not found" : airline}</p>
-        <p>Route: {route == null ? "Route not found" : route}</p>
+        <p>Route (ICAO): {route == null ? "Route not found" : route}</p>
+        <p>Route: {airportNames == "" ? "Route not found" : airportNames}</p>
         <p>Velocity: {(parseFloat(flight[9]) * 3.6).toFixed(3)} km/h</p>
         <p>Geographic Altitude: {flight[13]}</p>
         <p>Longitude / Latitude: {(parseFloat(flight[5]))} / {(parseFloat(flight[6]))}</p>
@@ -111,29 +149,38 @@ function App() {
 
   const [batchLoadingIndex, setbatchLoadingIndex] = useState(0);
   const [loadStartIndex, setLoadStartIndex] = useState(0);
+
+  const [loadedLiveFlights, setLoadedLiveFlights] = useState(false);
+
+  const loadUsingJSON = true;
+
+  const batchLoadingItemNum = 500;
   
   useEffect(() => {
-    /*
+    
     const getFlightsFromAPI = async() => {
-      const resp = await apiRequest();
-      setFlights(resp);
+      const resp = await apiRequest().then(out => {setFlights(out); setLoadedLiveFlights(true);});
     };
-    getFlightsFromAPI();*/
+    !loadUsingJSON && getFlightsFromAPI();
 
     const getFlightsFromJSON = jsonFlightData;
     const loadInit = async() => {
+      if (loadedLiveFlights === false && loadUsingJSON === false){
+        return;
+      }
       const tmp = getFlightsFromJSON.states.map(async(flight, index) => {
-        if (index < 1000){
+        if (index < batchLoadingItemNum){
           const resp = await getFlightRoutes(flight[1]);
           const airlineName = await getAirlineData(flight[1], resp);
-          return {...flight, callsign: resp, airline: airlineName};
+          const airportData = await getAirportData(resp);
+          return {...flight, callsign: resp, airline: airlineName, airportData: airportData};
         }
-        return {...flight, callsign: null, airline: null};
+        return {...flight, callsign: null, airline: null, airportData: null};
       });
       await Promise.all(tmp).then(out => {
         setFlights(out);
         setLoadComplete(true);
-        setLoadStartIndex(1000);
+        setLoadStartIndex(batchLoadingItemNum);
       });
     };
     loadInit();
@@ -145,17 +192,18 @@ function App() {
   useEffect(() => {
     const loadMoreData = async() => {
       const tmp = flights.map(async(flight, index) => {
-        if (index >= loadStartIndex & index < loadStartIndex+1000) {
+        if (index >= loadStartIndex & index < loadStartIndex + batchLoadingItemNum) {
           const resp = await getFlightRoutes(flight[1]);
           const airlineName = await getAirlineData(flight[1], resp);
-          return {...flight, callsign: resp, airline: airlineName};
+          const airportData = await getAirportData(resp);
+          return {...flight, callsign: resp, airline: airlineName, airportData: airportData};
         }
         return {...flight};
       });
       await Promise.all(tmp).then(out => {
         setFlights(out);
-        if (loadStartIndex + 1000 < flights.length){
-          setLoadStartIndex(loadStartIndex + 1000);
+        if (loadStartIndex + batchLoadingItemNum < flights.length){
+          setLoadStartIndex(loadStartIndex + batchLoadingItemNum);
           setbatchLoadingIndex(batchLoadingIndex+1);
         }
       });
