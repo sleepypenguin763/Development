ed987eca0c4c7c4f141948b9bafd50e987e81526
sleepypenguin763 <kenya_kimata+1300@brown.edu>
2022-11-17 22:28:14 -0500

    separated files pt 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 import { apiRequest, saveDataAsJSON, getFlightRoutes, getAirlineData, getAirportData } from "./APIRequests.js";
+import {SetupProgressBar} from "./ProgressBar.js";
 import { useCallback, useEffect, useState } from "react";
-import Slider, { SliderThumb } from "@mui/material/Slider";
-import { styled } from "@mui/material/styles";
+import Slider from "@mui/material/Slider";
 import jsonFlightData from "./assets/data.json";
-import FlightIcon from "@mui/icons-material/Flight";
 
 function CurrentAirplenStatus({ data, filter }) {
   let dataToRender = data;
@@ -79,53 +78,6 @@ function CurrentAirplenStatus({ data, filter }) {
       </div>
     );
   });
-}
-
-const CheckMarkSlider = styled(Slider)(({ theme }) => ({
-  color: "#3a8589",
-  height: 3,
-  padding: "13px 0",
-  "& .MuiSlider-thumb": {
-    height: 25,
-    width: 25,
-    backgroundColor: "#3a8589",
-    color: "#fff",
-    "& .checkmark-bar": {
-      height: 9,
-      width: 1,
-      backgroundColor: "currentColor",
-      marginLeft: 1,
-      marginRight: 1,
-    },
-  },
-  "& .MuiSlider-track": {
-    color: (theme.palette.mode = "#3a8589"),
-    height: 5,
-  },
-  "& .MuiSlider-rail": {
-    color: theme.palette.mode === "dark" ? "#3a8589" : "#d8d8d8",
-    opacity: theme.palette.mode === "dark" ? undefined : 1,
-    height: 5,
-  },
-}));
-
-function CheckMarkThumbComponent(props) {
-  const { children, ...other } = props;
-  return (
-    <SliderThumb {...other}>
-      {children}
-      <FlightIcon />
-    </SliderThumb>
-  );
-}
-
-function SetupProgressBar({ value }) {
-  //Loading screen credit: https://codesandbox.io/s/customizedslider-material-demo-forked-299xcn?fontsize=14&hidenavigation=1&theme=dark&file=/demo.tsx:32-91
-  return (
-    <div className="w-50 mx-auto mb-5">
-      <CheckMarkSlider value={value} disabled components={{ Thumb: CheckMarkThumbComponent }} />
-    </div>
-  );
 }
 
 function App() {
@@ -273,7 +225,7 @@ function App() {
   return (
     <div className="App">
       <h1>Flight Description</h1>
-      {<SetupProgressBar value={(100 * loadingProgress) / dataSize} />}
+      {(((100 * loadingProgress) / dataSize) < 99.5) && <SetupProgressBar value={(100 * loadingProgress) / dataSize} />}
 
       {loadComplete && <GetSlider />}
       {loadComplete === false ? (
---
+++ src/ProgressBar.js
@@ -0,0 +1,50 @@
+import { styled } from "@mui/material/styles";
+import FlightIcon from "@mui/icons-material/Flight";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+
+const CheckMarkSlider = styled(Slider)(({ theme }) => ({
+  color: "#3a8589",
+  height: 3,
+  padding: "13px 0",
+  "& .MuiSlider-thumb": {
+    height: 25,
+    width: 25,
+    backgroundColor: "#3a8589",
+    color: "#fff",
+    "& .checkmark-bar": {
+      height: 9,
+      width: 1,
+      backgroundColor: "currentColor",
+      marginLeft: 1,
+      marginRight: 1,
+    },
+  },
+  "& .MuiSlider-track": {
+    color: (theme.palette.mode = "#3a8589"),
+    height: 5,
+  },
+  "& .MuiSlider-rail": {
+    color: theme.palette.mode === "dark" ? "#3a8589" : "#d8d8d8",
+    opacity: theme.palette.mode === "dark" ? undefined : 1,
+    height: 5,
+  },
+}));
+
+function CheckMarkThumbComponent(props) {
+  const { children, ...other } = props;
+  return (
+    <SliderThumb {...other}>
+      {children}
+      <FlightIcon />
+    </SliderThumb>
+  );
+}
+
+export function SetupProgressBar({ value }) {
+  //Loading screen credit: https://codesandbox.io/s/customizedslider-material-demo-forked-299xcn?fontsize=14&hidenavigation=1&theme=dark&file=/demo.tsx:32-91
+  return (
+    <div className="w-50 mx-auto mb-5">
+      <CheckMarkSlider value={value} disabled components={{ Thumb: CheckMarkThumbComponent }} />
+    </div>
+  );
+}
